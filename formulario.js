712f32956bc4c5163d67118e5c581a7c76f76ad8
blogdopata <vcaballero_c@hotmail.com>
2015-02-03 17:46:31 -0500

    Empezando con @Prigazzi minuteo 30

--- formulario.js
+++ formulario.js
@@ -1,5 +1,6 @@
 var $form = $('#formulario'),
 	$titulo = $('#titulo'),
+	$autor = $('#autor'),
 	$url = $('#url'),
 	$button = $('#mostrar-form'),
 	$list = $('#contenido'),
@@ -14,9 +15,12 @@ function mostrarFormulario(){
 function agregarPost () {
 	var url = $url.val(),
 		titulo = $titulo.val(),
+		autor = $autor.val(),
 		$clone = $post.clone();
 
-	$clone.find('.titulo_item a').text(titulo).attr('href',url);
+	$clone.find('.titulo_item a').text(titulo).attr('href',url).attr('alt','lol');
+	
+	$clone.find('#lol').text(autor).attr('href','http://www.twitter.com');
 	
 	$clone.hide();
 
