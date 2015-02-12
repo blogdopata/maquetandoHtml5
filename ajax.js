ce33d6e4d08dd662c59647d052f404218f842e97
blogdopata <vcaballero_c@hotmail.com>
2015-02-12 17:49:18 -0500

    entrando AJAX

--- ajax.js
+++ ajax.js
@@ -1,6 +1,15 @@
 $(function(){
+	//$.ajax();
+	//codigo es un callback para que retorne algo 
+	//ya esta masticado  por ajax
 	$.get('logos_footer.html',function(codigo){
 		$('footer').append(codigo);
 	});
+
+	$.get('usuario.json', function(info){
+		console.log(info);
+
+	});
+
 
 });
--- geo.js
+++ geo.js
@@ -1,13 +1,28 @@
-var geo = navigator.geolocation;
+$(function(){
+	var geo = navigator.geolocation;
+	//console.log(geo)
+	var opciones = {};
+	function geo_error () {
+		console.log("Necesito las API's DE LA NSA");
+	}
+	function geo_exito(posicion){
+		console.log(posicion);
+		var lat  = posicion.coords.latitude;
+		var lon  = posicion.coords.longitude;
+		var lol  = posicion.timestamp;
+		/*console.log(lat + " y " + lon + " si " + lol);*/
+		var mapa = new Image() ;
+		mapa.src = "http://maps.googleapis.com/maps/api/staticmap?maptype=hybrid&zoom=20&size=300x300&sensor=false&center=" + lat + "," + lon;
+		$('#geo').append(mapa);
 
-function geo_error () {
-	console.log("no se donde estas");
-}
-function geo_exito(posicion){
-	console.log(posicion);
-}
-function opciones(){
-}
+		/*alert(mapa.src);*/
+	}
+	function opciones(){
+	}
 
-geo.getCurrentPosition(geo_exito,geo_error,opciones);
+	geo.getCurrentPosition(geo_exito,geo_error,opciones);
 
+
+
+});	
+	
